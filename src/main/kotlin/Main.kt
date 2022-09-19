39adb48b1062add17f2332f6e4e1becbfff56563
HalfloveJAR <29326375+HalfloveJAR@users.noreply.github.com>
2022-09-18 19:06:10 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,60 @@
+import java.io.FileWriter
+import java.lang.Exception
+
+/*
+ * Author: Halflove
+ * Title: pz-spawnpoint-gen
+ * Description: Project Zomboid spawn points lua file generator
+ */
+
+fun main(args: Array<String>) {
+
+    // Get user input for cell
+    println("Enter cell (ex: 20x33)")
+    var inputCell = readLine()!!.toString()
+    println("Cell: $inputCell")
+
+    // Get user input for rel
+    println("Enter rel (ex: 74x142)")
+    var inputRel = readLine()!!.toString()
+    println("Rel: $inputRel")
+
+    // User inputs desired name of spawn location
+    println("Enter desired spawn point name")
+    var inputName = readLine()!!.toString()
+    println("Name: $inputName")
+
+    // Call createSpawnPointsLua function
+    createSpawnPointsLua(inputCell, inputRel, inputName)
+}
+
+internal fun createSpawnPointsLua(cell: String, rel: String, name: String) {
+
+    // Split cell and rel input into separate ints
+    val delimiter = "x"
+    val cellParts = cell.split(delimiter)
+    val relParts = rel.split(delimiter)
+
+    var worldX: Int = cellParts[0].toInt()
+    var worldY: Int = cellParts[1].toInt()
+    var posX: Int = relParts[0].toInt()
+    var posY: Int = relParts[1].toInt()
+
+    // create lua file
+    try {
+
+        var spawnPointFile = FileWriter(name + "_spawnpoints.lua")
+        spawnPointFile.write("function SpawnPoints()\n" +
+            "    return {\n" +
+            "        unemployed = {\n" +
+            "            { worldX = $worldX, worldY = $worldY, posX = $posX, posY = $posY }\n" +
+            "        }\n" +
+            "    }\n" +
+            "end")
+        spawnPointFile.close()
+
+    } catch(ex:Exception) {
+        print(ex.message)
+    }
+
+}
